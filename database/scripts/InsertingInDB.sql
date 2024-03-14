06310cbaf09aac7ad9ca92997c44a2edec95e666
Ole Marcus Aarnes <omaarnes@ntnu.nom>
2024-03-14 19:17:01 +0100

    Endret på begge Insert-scriptet grunnet redudans i koden.

--- database/scripts/InsertingInDB.sql
+++ database/scripts/InsertingInDB.sql
@@ -1,23 +1,23 @@
 -- Script til å legge til de nødvendige verdiene inn i databasen.
 
 --Sal
-INSERT INTO Sal values (1, 524, 'Hovedscenen');
-INSERT INTO Sal values (2, 332, 'Gamle Scene');
+INSERT INTO Sal VALUES (1, 524, 'Hovedscenen'),
+(2, 332, 'Gamle Scene');
 
 --Teaterstykke
-INSERT INTO Teaterstykke values (1, 'Kongsemnene');
-INSERT INTO Teaterstykke values (2, 'Størst av alt er kjærligheten');
+INSERT INTO Teaterstykke VALUES (1, 'Kongsemnene'), 
+(2, 'Størst av alt er kjærligheten');
 
 --Rolle
-INSERT INTO Rolle values (1, 'Haakon Haakonssønn');
-INSERT INTO Rolle values (2, 'Inga fra Vartjeg');
-INSERT INTO Rolle values (3, 'Skule Jarl');
-INSERT INTO Rolle values (4, 'Fru Ragnhild');
-INSERT INTO Rolle values (5, 'Margrete');
-INSERT INTO Rolle values (6, 'Sigrid');
-INSERT INTO Rolle values (7, 'Biskop Nicolas');
-INSERT INTO Rolle values (8, 'Gregorius Jonssønn');
-INSERT INTO Rolle values (9, 'Paal Flida');
-INSERT INTO Rolle values (10, 'Baard Bratte');
-INSERT INTO Rolle values (11, 'Jatgeir Skald');
-INSERT INTO Rolle values (12, 'Peter');
+INSERT INTO Rolle VALUES (1, 'Haakon Haakonssønn'),
+ (2, 'Inga fra Vartjeg'),
+ (3, 'Skule Jarl'),
+ (4, 'Fru Ragnhild'),
+ (5, 'Margrete'),
+ (6, 'Sigrid'),
+ (7, 'Biskop Nicolas'),
+ (8, 'Gregorius Jonssønn'),
+ (9, 'Paal Flida'),
+ (10, 'Baard Bratte'),
+ (11, 'Jatgeir Skald'),
+ (12, 'Peter');
