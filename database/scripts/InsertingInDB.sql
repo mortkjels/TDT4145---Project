309ce8a4ef8ad408c34e7c465619d38653b6f7bf
Ole Marcus Aarnes <omaarnes@ntnu.nom>
2024-03-14 18:42:08 +0100

    Flyttet om på data inne i scriptfilene.

--- database/scripts/InsertingInDB.sql
+++ database/scripts/InsertingInDB.sql
@@ -1,1 +1,23 @@
 -- Script til å legge til de nødvendige verdiene inn i databasen.
+
+--Sal
+INSERT INTO Sal values (1, 524, 'Hovedscenen');
+INSERT INTO Sal values (1, 332, 'Gamle Scene');
+
+--Teaterstykke
+INSERT INTO Teaterstykke values (1, 'Kongsemnene')
+INSERT INTO Teaterstykke values (2, 'Størst av alt er kjærligheten')
+
+--Rolle
+INSERT INTO Rolle values (1, 'Haakon Haakonssønn')
+INSERT INTO Rolle values (2, 'Inga fra Vartjeg')
+INSERT INTO Rolle values (3, 'Skule Jarl')
+INSERT INTO Rolle values (4, 'Fru Ragnhild')
+INSERT INTO Rolle values (5, 'Margrete')
+INSERT INTO Rolle values (6, 'Sigrid')
+INSERT INTO Rolle values (7, 'Biskop Nicolas')
+INSERT INTO Rolle values (8, 'Gregorius Jonssønn')
+INSERT INTO Rolle values (9, 'Paal Flida')
+INSERT INTO Rolle values (10, 'Baard Bratte')
+INSERT INTO Rolle values (11, 'Jatgeir Skald')
+INSERT INTO Rolle values (12, 'Peter')
